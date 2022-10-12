ae4bc4818df18c7f296b586fa7f88eb6d1eab4aa
PathumSanjana <94284049+PathumSanjana@users.noreply.github.com>
2022-10-12 09:01:26 +0000

    Add files via upload

--- loader.s
+++ loader.s
@@ -13,5 +13,14 @@
 
     loader:                         ; the loader label (defined as entry point in linker script)
         mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+         ; The assembly code
+    extern sum_of_three   ; the function sum_of_three is defined elsewhere
+
+    push dword 3            ; arg3
+    push dword 2            ; arg2
+    push dword 1            ; arg1
+    call sum_of_three       ; call the function, the result will be in eax
+    
+    
     .loop:
         jmp .loop                   ; loop forever
