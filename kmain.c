491a7e6742ee8ee7baa33e8e33c897d750bfb891
PathumSanjana <94284049+PathumSanjana@users.noreply.github.com>
2022-11-09 11:51:33 +0000

    integrate_outputs

--- kmain.c
+++ kmain.c
@@ -1,5 +1,18 @@
-    /* The C function */
-    int sum_of_three(int arg1, int arg2, int arg3)
+   #include "drivers/frame_buffer.h"
+   #include "drivers/serial_port.h"
+
+    void kmain()
     {
-        return arg1 + arg2 + arg3;
+       char arr[] = "Welcome to My OS";
+       fb_write(arr, sizeof(arr));
+       serial_configure(SERIAL_COM1_BASE, Baud_115200);
+       serial_write(SERIAL_COM1_BASE, arr, sizeof(arr));
+
+       //serial_write(0x3F8, arr, 4);
+       
+
+       // fb_write_cell(0, 'A', FB_GREEN, FB_DARK_GREY);
+       //fb_move_cursor(00);
     }
+    
+    
